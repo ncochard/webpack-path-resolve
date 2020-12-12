7283b36157fa844fffa7e76f239858d30034ba17
Nicolas Cochard <nicolas@cochard.co.uk>
2020-12-12 00:51:31 +0000

    initial commit

--- packages/webpack-path-resolve-tests/src/test.ts
+++ packages/webpack-path-resolve-tests/src/test.ts
@@ -5,14 +5,12 @@ const resolve = webpackPath.resolve(require.resolve.paths);
 
 test("jest should be found at the root of the mono repo", () => {
   const actual = resolve("jest");
-  console.log(actual);
   const expected = path.resolve("../../node_modules/jest");
   expect(actual).toEqual(expected);
 });
 
 test("@types/jest should be found in this package", () => {
   const actual = resolve("@types/jest");
-  console.log(actual);
   const expected = path.resolve("./node_modules/@types/jest");
   expect(actual).toEqual(expected);
 });
